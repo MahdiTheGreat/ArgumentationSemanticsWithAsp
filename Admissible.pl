ccdfa45e22734328c7c862acdd1b5ee5ae42ba4f
B1naryB0t <47212121+MahdiTheGreat@users.noreply.github.com>
2023-12-08 17:27:05 +0330

    Update Admissible.pl

--- Admissible.pl
+++ Admissible.pl
@@ -4,6 +4,10 @@
 not_selected_arg(X):-arg(X),not selected_arg(X).
 
 % Test for admissible
+%Notice that we use the same line we used in the Conflict-free program and that is because a stable set has to be at first conflict-free. 
+% we also check if there is an unselected argument that isn't attacked by at least one of our selected arguments. 
+% With this program we find sets of arguments that attack all the unselected arguments and in a way this extension is more conservative 
+% and follows the idea of "You are either with us, or against us".
 :- selected_arg(X),selected_arg(Y),att(X,Y).
 defends_against(X,Y,Z):-arg(X),arg(Y),arg(Z),att(Z,Y),att(X,Z).
 :-selected_arg(X),not_selected_arg(Z),att(Z,X),not 1{defends_against(Y,X,Z):selected_arg(Y)}.
