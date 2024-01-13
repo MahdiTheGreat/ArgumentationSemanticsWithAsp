632f81f796bcb6aa8b5f191bd6da7e6e92596ca6
B1naryB0t <47212121+MahdiTheGreat@users.noreply.github.com>
2024-01-14 02:48:00 +0330

    Update Preferred.pl
    the perv code didn't work with a cyclical graph but the new code works because it's essentially using the code of the admissible program with a condition that it's trying to minimize.

--- Preferred.pl
+++ Preferred.pl
@@ -1,6 +1,8 @@
 
+
 % Generate
 {selected_arg(X):arg(X)}:-.
+
 % the reason we didn't just use ' not selected_arg(X)' is because we would get unsafe error
 not_selected_arg(X):-arg(X),not selected_arg(X).
 
@@ -8,7 +10,8 @@ not_selected_arg(X):-arg(X),not selected_arg(X).
 :- selected_arg(X),selected_arg(Y),att(X,Y).
 defends_against(X,Y,Z):-arg(X),arg(Y),arg(Z),att(Z,Y),att(X,Z).
 :-selected_arg(X),not_selected_arg(Z),att(Z,X),not 1{defends_against(Y,X,Z):selected_arg(Y)}.
-:-not_selected_arg(X),not att(X,X),not 1{att(X,Y):selected_arg(Y)},not 1{att(Y,X):selected_arg(Y)}.
+%:-not_selected_arg(X),not att(X,X),not 1{att(X,Y):selected_arg(Y)},not 1{att(Y,X):selected_arg(Y)}.
+preferred_condition(X):-not_selected_arg(X),not att(X,X),not 1{att(X,Y):selected_arg(Y)},not 1{att(Y,X):selected_arg(Y)}.
 
 % Arguments
 arg(1..4).
@@ -20,6 +23,9 @@ att(1,2).
 att(1,4).  
 att(2,1).
 %att(1,1).
+
+
+#minimize {X:preferred_condition(X)}.
 
 #show selected_arg/1.
 %#show not_selected_arg/1. 
